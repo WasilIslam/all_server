56c9f7bade7d84697ae501c1dd78e5fbc27a026d
wasilislam <wasilislam456@gmail.com>
2021-12-19 16:24:53 +0500

    first commit

--- routes/cookie.js
+++ routes/cookie.js
@@ -5,13 +5,14 @@ router.get("/info", async (req, res) => {
   res.json("Sends cookie all-auth-token");
 });
 router.get("/", async (req, res) => {
+    const cookies = req.cookies;
   console.log("getting the cookie from the cookie parser");
-  console.log("Cookies found: "+ req.cookies)
+  console.log("Cookies found: ", cookies)
   res
     .cookie("all-auth-token", "working", {
       maxAge: 86_400_000,
       httpOnly: true,
     })
-    .send(req.cookies);
+    .send(cookies);
 });
 module.exports = router;
